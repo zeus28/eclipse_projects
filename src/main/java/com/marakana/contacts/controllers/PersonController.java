87789b4da3edf33449a316bd1f2e15ea5c3ca344
kyriakosz <kyriakosz@datatech.com.cy>
2015-12-15 12:38:43 +0200

    ok

--- src/main/java/com/marakana/contacts/controllers/PersonController.java
+++ src/main/java/com/marakana/contacts/controllers/PersonController.java
@@ -1,18 +1,22 @@
 package com.marakana.contacts.controllers;
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import com.marakana.contacts.repositories.ContactRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.marakana.contacts.entities.Address;
+
+import com.marakana.contacts.entities.Person;
+import com.marakana.contacts.repositories.PersonRepository;
 
 @Controller
-public class ContactController {
+public class PersonController {
 
 	@Autowired
-	private ContactRepository contactRepository;
+	private PersonRepository personRepository;
 
 	/*
 	 * @RequestMapping(value = "/contacts", method = RequestMethod.GET) public
@@ -26,12 +30,58 @@ public class ContactController {
 	 * 
 	 * }
 	 */
-	@RequestMapping(value = "/contacts", method = RequestMethod.GET)
-	public String getContactList(Model model) {
-		model.addAttribute("contacts", contactRepository.findAll());
-		return "contact/list";
+
+
+	@RequestMapping(value = "/person", params = "add", method = RequestMethod.GET)
+	public String getAddContact() {
+
+		return "person/add";
+
 	}
 
+	@RequestMapping(value = "/person", params = "edit", method = RequestMethod.GET)
+	public String getEditContact(@RequestParam long id, Model model) {
+		model.addAttribute("person", personRepository.findOne(id));
+		return "person/edit";
+
+	}
+
+	@RequestMapping(value = "/person", method = RequestMethod.GET)
+	public String getViewContact(@RequestParam long id, Model model) {
+		model.addAttribute("person", personRepository.findOne(id));
+		return "person/view";
+
+	}
+
+	@RequestMapping(value = "/person", params = "add", method = RequestMethod.POST)
+	public String postAddPerson(@RequestParam String name, @RequestParam String street, @RequestParam String city,
+			@RequestParam String state, @RequestParam String zip) {
+		Address address = new Address(street, city, state, zip);
+		Person person = new Person(name, address);
+		person=personRepository.save(person);
+		return "redirect:person?id=" + person.getId();
+
+	}
+	@RequestMapping(value = "/person", params = "edit", method = RequestMethod.POST)
+	public String postEditPerson(@RequestParam Long id,@RequestParam String name, 
+			@RequestParam String street, 
+			@RequestParam String city,
+			@RequestParam String state, @RequestParam String zip) {
+		Person  person = personRepository.findOne(id);
+		Address address= person.getAddress();
+		address.setCity(city);
+		address.setState(state);
+		address.setStreet(street);
+		address.setZip(zip);
+		person.setName(name);
+		personRepository.save(person);
+		return "redirect:person?id=" + person.getId();
+	}
+	@RequestMapping(value = "/person", params = "delete", method = RequestMethod.POST)
+	public String postDeletePerson(@RequestParam Long id) {
+		personRepository.delete(id);
+		return "redirect:contacts";
+	}
 
 
 
